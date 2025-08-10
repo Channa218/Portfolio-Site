03bbaf1effc52988cddc1e212c008a8078b8cfb7
Channa <channavimukthi21871@gmaail.com>
2025-08-10 19:18:38 +0530

    create the navigation bar section

--- portfoliyo/src/App.js
+++ portfoliyo/src/App.js
@@ -1,10 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Navbar from "./Components/NavBar/navbar";
 function App() {
   return (
     <div className="App">
-      
+      <Navbar />
     </div>
   );
 }
---
+++ portfoliyo/src/Components/NavBar/navbar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './navbar.css';
+import logo from '../../assets/logo.png'; 
+import { Link } from 'react-scroll'
+import ContactImg from '../../assets/contact.png';
+
+const Navbar = () => {
+  return (
+        <nav className="navbar">
+            <img src={logo} alt="Logo" className="logo" />
+            <div className="desktopMenu">
+                <Link className="desktopMenuListItem">Home</Link>
+                <Link className="desktopMenuListItem">About</Link>
+                <Link className="desktopMenuListItem">Portfoliyo</Link>
+                <Link className="desktopMenuListItem">Clients</Link>   
+            </div>
+            <button className="desktopMenuBtn">
+                <img src={ContactImg} alt="" className="desktopMenuImg"/> Contact 
+            </button>
+        </nav>
+  )
+}
+
+export default Navbar;
